28527853901088c1d9648774d98e248ff810f22f
Vadym Pavlyk <madeira@Vadyms-Laptop.local>
2024-11-03 14:06:22 +0200

    feat: add screen reader support

--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -23,7 +23,12 @@ function ToastShelf() {
   }, [dismissAllToasts]);
 
   return (
-    <ol className={styles.wrapper}>
+    <ol
+      className={styles.wrapper}
+      role="region"
+      aria-live="polite"
+      aria-label="Notification"
+    >
       {toasts.map(({ id, variant, message }) => (
         <li key={id} className={styles.toastWrapper}>
           <Toast id={id} variant={variant}>
